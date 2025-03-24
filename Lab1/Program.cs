91a6c454b99abd8f41a991d63eb5cb0ac378b850
kerryannzee <kerryannashburn@gmail.com>
2025-03-24 23:49:42 +0000

    Commit these changes

---
+++ Lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Step 1
+        Console.WriteLine("Kerry Ashburn IT-1050-Programming Logic");
+
+        //Step 2
+        /*
+        Name: [Kerry Ashburn]
+        Title: [IT-1050 - Lab 1]
+        */ 
+
+        //Step 3
+        int myFavNum = 17;
+        Console.WriteLine("My favorite number is "+myFavNum+"");
+
+        string myfavLanguage = "SQL";
+        Console.WriteLine("My favorite programming language is "+myfavLanguage+"");
+
+        double Nada = 0;
+        Console.WriteLine("The number of programs I have written is "+Nada+"");
+
+        bool HasProgrammingExperience = false;
+        Console.WriteLine("The statement 'I have programming experience' is "+HasProgrammingExperience+"");
+
+        //Step 5
+        const string mySchool = "Tri-C";
+        Console.WriteLine("My school name is "+mySchool+"");
+    }
+}
