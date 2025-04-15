51969a114ced64067c5f1a489f22fa844f4cf1e4
kerryannzee <kerryannashburn@gmail.com>
2025-04-15 23:23:18 +0000

    Commit these changes

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,9 +1,61 @@
 ﻿namespace Lab4;
 
-class Program
-{
+      class Program
+  {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+   //Problem 1
+      for (int i = 0; i < 11; i++) 
+      {
+        Console.WriteLine(i);
+      }    
+  //Problem 2
+      
+      for (int i = 1; i <= 20; i++) 
+
+      if (i % 2 == 0) 
+      {
+        Console.WriteLine(i);
+      }
+      
+  //Problem 3
+      int j = 5;
+
+      while (j >= 1)
+      {
+      Console.WriteLine(j);
+      j--; 
+      }
+      
+  //Problem 4
+      int number;
+
+      do
+      {
+      Console.WriteLine("Please enter a number greater than 100:");
+      number = Convert.ToInt32(Console.ReadLine());
+      } while (number <= 100);
+
+      Console.WriteLine($"You entered a valid number: {number}");
+      
+  //Problem 5
+      int k = 10;
+
+      while (k <= 1000)
+      {
+      Console.WriteLine(k);
+      k += 10; // Increment by 10
+      } 
+      
+  //Problem 6
+      for (int l = 1; l <= 10; l++) // Outer loop controls the rows
+      {
+      for (int m = 1; m <= l; m++) // Inner loop controls the stars per row
+      {
+      Console.Write("*");
+      }
+      Console.WriteLine(); // Move to the next line after each row
+      }
     }
+  }
 }
--- Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
+++ Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c82aad449a05e555ce988d6e9e775e4667084d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75814ca2cab66eaf9b899b64a6b4983728479e84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
