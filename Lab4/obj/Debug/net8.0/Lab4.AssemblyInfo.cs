da66f7b6c3410ee7bb6ff229c7ea646c061dd0af
kerryannzee <kerryannashburn@gmail.com>
2025-04-22 00:55:18 +0000

    commit these changes

--- Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
+++ Lab4/obj/Debug/net8.0/Lab4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75814ca2cab66eaf9b899b64a6b4983728479e84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7cfcdcf965ca041d51838c1524c1a9d1ab5caa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
