1a45f888535cbb0f6fa41d513e2f281512560847
kerryannzee <kerryannashburn@gmail.com>
2025-04-23 00:51:22 +0000

    commit these changes

--- Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9095d7c7d249da08c79fe289358db2b130f728b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f3eccaf31a9fca340f9d78bd597bf3077ba2397")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab5/obj/Debug/net8.0/Lab5.AssemblyInfo.cs
+++ Lab5/obj/Debug/net8.0/Lab5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1344930b1a33284a59f11fbdfe9bb7ecdd3565c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f3eccaf31a9fca340f9d78bd597bf3077ba2397")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
