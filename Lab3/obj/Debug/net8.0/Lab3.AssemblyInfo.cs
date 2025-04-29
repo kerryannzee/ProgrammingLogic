e407b07525aedb564abad3ffe258e67382a3a2cf
kerryannzee <kerryannashburn@gmail.com>
2025-04-29 23:29:23 +0000

    commit these changes

--- Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f655456a4da27405dafe730f92f9c496c605146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06268a75554599b75a97454e73c8a8b6a22817b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab6/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
